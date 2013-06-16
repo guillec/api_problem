dffcb9c256d10c0c8f9da345a8f9bd89951003f2
Guille Carlos <guille@bitpop.in>
2013-06-16 17:51:41 -0400

    Change whitespec to fource a travis ci test

--- test/api_problem_test.rb
+++ test/api_problem_test.rb
@@ -2,7 +2,6 @@ require "test_helper.rb"
 require "api/problem"
 
 describe "Api::Problem module" do
-
   it "should return a hash" do
 
     fake_class = class Class
@@ -12,5 +11,4 @@ describe "Api::Problem module" do
     http_problem = { problemType: "http://example.com", title: "This is a test" }
     assert_equal http_problem, fake_class.new.api_problem("http://example.com", "This is a test")
   end
-
 end
